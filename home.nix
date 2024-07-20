6bd2cc38a07784b8fe4d0e833ed12d213f7af639
0xkowalski1 <0xkowalskiaudit@gmail.com>
2024-07-20 23:05:46 +0100

    Added font/config for alacritty

--- home.nix
+++ home.nix
@@ -6,22 +6,43 @@
 
   home.stateVersion = "24.05"; # DO NOT CHANGE
 
+  nixpkgs.config.allowUnfree = true;
+
   home.packages = with pkgs; [
   	htop
 	xclip
 	dmenu
+	brave
+	acpi
+	neovim
+	spotify
+	discord
+
+	# Font
+	(nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
-
-  home.file = {
-  };
-
-  home.sessionVariables = {
-  };
 
   programs.git = {
   	enable = true;
 	userEmail = "0xkowalskiaudit@gmail.com";
 	userName = "0xkowalski1";
+  };
+
+  # Font
+  fonts.fontconfig.enable = true;
+
+  # Terminal
+  programs.alacritty = {
+  	enable = true;
+	settings = { 
+		font = {
+			normal = {
+				family = "FiraCode Nerd Font Mono";
+				style = "regular";
+			};
+			size = 12.0;
+		};
+	};
   };
 
   programs.home-manager.enable = true;
