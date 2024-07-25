d1c1e13eb7d8d8007b459de435e4215d2a56fbb4
warsmite <warsmite@proton.me>
2024-07-25 07:43:48 +0100

    Added a few lsps

--- Neovim/lua/plugins/lsp_config.lua
+++ Neovim/lua/plugins/lsp_config.lua
@@ -1,1 +1,17 @@
-require("lspconfig").lua_ls.setup{} 
+local lspconfig = require("lspconfig")
+
+-- Lua
+lspconfig.lua_ls.setup({})
+
+-- Go
+lspconfig.gopls.setup({})
+
+-- Html
+lspconfig.html.setup({})
+
+-- Css
+lspconfig.cssls.setup({})
+
+-- Json
+lspconfig.jsonls.setup({})
+
