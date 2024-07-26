d39d14488a1ef5478b84058614b3be7988f056e2
0xkowalski1 <0xkowalskiaudit@gmail.com>
2024-07-26 03:12:40 +0100

    Added unclutter to hide mouse cursor, added script to toggle trackpad on laptop

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports =
@@ -15,12 +15,30 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # Disable Trackpad
+  # Disable trackpad by default
   services.xserver.displayManager.sessionCommands = ''
     xinput disable "bcm5974"
+    unclutter &
   '';
 
+  # Script for toggling trackpad
+  environment.systemPackages = with pkgs; [
+    unclutter # For hiding cursor
 
+    (pkgs.writeScriptBin "toggleTrackpad" ''
+      #!/bin/sh
+      TRACKPAD_NAME="bcm5974" 
+      TRACKPAD_ID=$(xinput list --id-only "$TRACKPAD_NAME")
+
+      if [ "$(xinput list-props "$TRACKPAD_ID" | grep 'Device Enabled' | awk '{print $4}')" -eq 1 ]; then
+          xinput disable "$TRACKPAD_ID"
+          unclutter &
+      else
+          xinput enable "$TRACKPAD_ID"
+          pkill unclutter
+      fi
+    '')
+  ];
 
   system.stateVersion = "24.05"; # DO NOT CHANGE
 }
