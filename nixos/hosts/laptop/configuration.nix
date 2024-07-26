0b2ce52ce8b612de24f8add2961142340b9ae57c
0xkowalski1 <0xkowalskiaudit@gmail.com>
2024-07-26 02:17:39 +0100

    Added potential fix for macbook touchpad being too sensitive

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -15,5 +15,10 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
+  # Trackpad
+  services.libinput.touchpad.disableWhileTyping = true;
+
+
+
   system.stateVersion = "24.05"; # DO NOT CHANGE
 }
