bd4416e74b26d9db67ac720864ae102009edf10d
0xkowalski1 <0xkowalskiaudit@gmail.com>
2024-08-15 05:15:15 +0100

    Filled out laptops home.nix

---
+++ nixos/hosts/laptop/home.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  spotifyPlayer.enable = true;
+  brave.enable = true;
+  neovim.enable = true;
+  alacritty.enable = true;
+  bash.enable = true;
+  git.enable = true;
+}
