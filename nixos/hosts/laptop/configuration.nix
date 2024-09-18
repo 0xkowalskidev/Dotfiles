c8ce9c7f7446fa8fa294cf8f1bea1dd20a759632
0xkowalski1 <0xkowalskiaudit@gmail.com>
2024-09-18 17:08:33 +0100

    Changed toggle trackpad logic to work with sway, removed unclutter as sway has support for hiding cursor

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -33,31 +33,6 @@
 
   projects.container-orchestrator.enable = true;
 
-  # Disable trackpad by default
-  #  services.xserver.displayManager.sessionCommands = ''
-  #   xinput disable "bcm5974"
-  #   unclutter &
-  # '';
-
-  # Script for toggling trackpad
-  environment.systemPackages = with pkgs; [
-    unclutter # For hiding cursor
-
-    (pkgs.writeScriptBin "toggleTrackpad" ''
-      #!/bin/sh
-      TRACKPAD_NAME="bcm5974" 
-      TRACKPAD_ID=$(xinput list --id-only "$TRACKPAD_NAME")
-
-      if [ "$(xinput list-props "$TRACKPAD_ID" | grep 'Device Enabled' | awk '{print $4}')" -eq 1 ]; then
-          xinput disable "$TRACKPAD_ID"
-          unclutter &
-      else
-          xinput enable "$TRACKPAD_ID"
-          pkill unclutter
-      fi
-    '')
-  ];
-
   # Home Manager
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
