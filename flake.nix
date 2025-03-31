fcc5eb7ab0a7dd1b3548657671a4c5f2dc6df7d5
0xkowalskidev <0xkowalskidev@gmail.com>
2025-03-31 16:33:47 +0100

    Added nixpkgs-stable for when packages are broken in unstable, reenabled container orchestrator with stable etcd on desktop

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -14,40 +15,59 @@
     nopswd.url = "github:0xkowalskidev/nopswd";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-stable, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config = { allowUnfree = true; }; # Optional: match your main nixpkgs config if needed
+      };
+    in
+    {
+      nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+        };
+        modules = [
+          ./nixos/hosts/laptop/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
 
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/hosts/laptop/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+      nixosConfigurations.chuwi = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+        };
+        modules = [
+          ./nixos/hosts/chuwi/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+        };
+        modules = [
+          ./nixos/hosts/desktop/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      nixosConfigurations.dopey = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+        };
+        modules = [
+          ./nixos/hosts/server-dopey/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      homeManagerModules.default = ./home-manager;
     };
-
-    nixosConfigurations.chuwi = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/hosts/chuwi/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-    };
-
-    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/hosts/desktop/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-    };
-
-    nixosConfigurations.dopey = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/hosts/server-dopey/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-    };
-
-    homeManagerModules.default = ./home-manager;
-  };
 }
