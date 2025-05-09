56c58cfd6ffd230661bccfa0fae4626fb85620a0
0xkowalskidev <0xkowalskidev@gmail.com>
2025-05-09 13:11:42 +0100

    Moved ddns and docker registry from dopey to sleepy

--- nixos/hosts/server-sleepy/configuration.nix
+++ nixos/hosts/server-sleepy/configuration.nix
@@ -20,6 +20,34 @@
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = true;
 
+  # DDNS
+  services.ddclient = {
+    enable = true;
+    interval = "1min";
+    protocol = "cloudflare";
+    ssl = true;
+    username = "token";
+    passwordFile = "/etc/nixos/ddns-token";
+    zone = "0xkowalski.dev";
+    domains = [ "@" ];
+  };
+
+  # Container Registry
+  services.dockerRegistry = {
+    enable = true;
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."registry.0xkowalski.dev" = {
+      extraConfig = ''
+        reverse_proxy localhost:5000
+      '';
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
   # Home Manager
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
