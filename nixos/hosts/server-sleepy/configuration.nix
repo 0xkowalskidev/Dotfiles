88f15b5fb1089d561dc6d73b3a897b6879f59d2c
warsmite <warsmite@proton.me>
2025-05-09 13:01:39 +0100

    Added server-sleepy as a host

---
+++ nixos/hosts/server-sleepy/configuration.nix
@@ -0,0 +1,38 @@
+{ inputs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../common.nix
+    ];
+
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "sleepy";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+
+  # Home Manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+
+    users = {
+      "kowalski" = { ... }: {
+        imports = [
+          ./home.nix
+          inputs.self.outputs.homeManagerModules.default
+        ];
+      };
+    };
+  };
+
+  system.stateVersion = "24.05"; # DO NOT CHANGE
+}
