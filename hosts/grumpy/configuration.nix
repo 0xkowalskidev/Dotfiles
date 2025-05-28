4aa73bb1e1026a0fdc91fd489f23e8dc9812b272
0xkowalskidev <0xkowalskidev@gmail.com>
2025-05-28 16:55:24 +0100

    Added server configs back after rewrite

---
+++ hosts/grumpy/configuration.nix
@@ -0,0 +1,36 @@
+{ inputs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ../../common.nix ];
+
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "grumpy";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+
+  # NAS
+  boot.supportedFilesystems = [ "nfs" ];
+
+  fileSystems."/mnt/data" = {
+    device = "192.168.1.129:/data";
+    fsType = "nfs";
+    options = [ "rw" "sync" ];
+  };
+
+  # User
+  users.users.kowalski = {
+    isNormalUser = true;
+    home = "/home/kowalski";
+    extraGroups = [ "wheel" ];
+  };
+
+  system.stateVersion = "25.05";
+}
---
+++ hosts/sleepy/configuration.nix
@@ -0,0 +1,48 @@
+{ inputs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ../../common.nix ];
+
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "sleepy";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+
+  # NAS
+  boot.supportedFilesystems = [ "nfs" ];
+
+  fileSystems."/mnt/data" = {
+    device = "192.168.1.129:/data";
+    fsType = "nfs";
+    options = [ "rw" "sync" ];
+  };
+
+  # DDNS
+  services.ddclient = {
+    enable = true;
+    interval = "1min";
+    protocol = "cloudflare";
+    ssl = true;
+    username = "token";
+    passwordFile = "/etc/nixos/ddns-token";
+    zone = "0xkowalski.dev";
+    domains = [ "@" ];
+  };
+
+  # User
+  users.users.kowalski = {
+    isNormalUser = true;
+    home = "/home/kowalski";
+    extraGroups = [ "wheel" ];
+  };
+
+  system.stateVersion = "25.05";
+}
