2020b77840ad98278072fdbb7264546a4aea721f
warsmite <warsmite@proton.me>
2025-05-28 03:58:49 +0100

    Setup games to start with mullvad-exclude

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -2,4 +2,32 @@
 
 {
   nixpkgs.config.allowUnfree = true;
+
+  # Override .desktop files to prepend mullvad-exclude
+  xdg.desktopEntries = {
+    steam = {
+      name = "Steam";
+      exec = "mullvad-exclude steam %U";
+      icon = "steam";
+      categories = [ "Network" "FileTransfer" "Game" ];
+      genericName = "Steam";
+      comment = "Application for managing and playing games on Steam";
+    };
+    lutris = {
+      name = "Lutris";
+      exec = "mullvad-exclude lutris %U";
+      icon = "lutris";
+      categories = [ "Game" ];
+      genericName = "Gaming Platform";
+      comment = "Open gaming platform";
+    };
+    "org.prismlauncher.PrismLauncher" = {
+      name = "PrismLauncher";
+      exec = "mullvad-exclude prismlauncher %U";
+      icon = "prismlauncher";
+      categories = [ "Game" ];
+      genericName = "Minecraft Launcher";
+      comment = "A custom launcher for Minecraft";
+    };
+  };
 }
