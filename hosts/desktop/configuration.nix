158cb2d672013f7a2c27a429aa110b37b74aee03
warsmite <warsmite@proton.me>
2025-05-29 20:35:14 +0100

    enabled 32 bit graphics

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -40,8 +40,11 @@
     };
   };
 
-  # Nvidia
-  hardware.graphics.enable = true;
+  # Nvidia + Graphics
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
   hardware.nvidia = {
     modesetting.enable = true;
     open = false;
