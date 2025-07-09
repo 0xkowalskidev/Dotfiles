207ccb4d683aa98ee04fa99f9fde7342a21d929b
warsmite <warsmite@proton.me>
2025-07-09 11:27:08 +0100

    Added ace

---
+++ hosts/ace/home.nix
@@ -0,0 +1,51 @@
+{ ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  # Monitors
+  wayland.windowManager.hyprland.settings = {
+    monitor =
+      [ "HDMI-A-1, 1920x1080, 0x0, 1" "DVI-D-1, 1920x1080, -1920x0, 1" ];
+    workspace = [
+      "1, monitor:HDMI-A-1, default:true"
+      "2, monitor:HDMI-A-1"
+      "3, monitor:HDMI-A-1"
+      "4, monitor:HDMI-A-1"
+      "5, monitor:HDMI-A-1"
+      "6, monitor:HDMI-A-1"
+      "7, monitor:HDMI-A-1"
+      "8, monitor:HDMI-A-1"
+      "9, monitor:HDMI-A-1"
+      "10, monitor:DVI-D-1"
+    ];
+  };
+
+  # Override .desktop files to prepend mullvad-exclude
+  xdg.desktopEntries = {
+    steam = {
+      name = "Steam";
+      exec = "mullvad-exclude steam %U";
+      icon = "steam";
+      categories = [ "Network" "FileTransfer" "Game" ];
+      genericName = "Steam";
+      comment = "Application for managing and playing games on Steam";
+    };
+    lutris = {
+      name = "Lutris";
+      exec = "mullvad-exclude lutris %U";
+      icon = "lutris";
+      categories = [ "Game" ];
+      genericName = "Lutris";
+      comment = "Open gaming platform";
+    };
+    "org.prismlauncher.PrismLauncher" = {
+      name = "PrismLauncher";
+      exec = "mullvad-exclude prismlauncher %U";
+      icon = "prismlauncher";
+      categories = [ "Game" ];
+      genericName = "Minecraft Launcher";
+      comment = "A custom launcher for Minecraft";
+    };
+  };
+}
