dcd4de45b3b4a0baad8610e48a693e6e0ba10715
0xkowalskidev <0xkowalskidev@gmail.com>
2026-01-13 18:15:08 +0000

    Added flatpack and xdg portal setup

--- home.nix
+++ home.nix
@@ -825,5 +825,20 @@
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+
+    config.common.default = [
+      "gtk"
+      "hyprland"
+    ];
+  };
+
   home.stateVersion = "25.05";
 }
