1046f67e643b2bf618fe9a7e55612ccd77ab7bd2
warsmite <warsmite@proton.me>
2026-03-08 14:17:09 +0000

    Moved battery waybar config to chuwi, improved asthetics for waybar/wofi

--- hosts/chuwi/home.nix
+++ hosts/chuwi/home.nix
@@ -4,11 +4,48 @@
   nixpkgs.config.allowUnfree = true;
 
   # Fix rotation in hyprland
-  wayland.windowManager.hyprland.settings.monitor = [ "DSI-1,preffered,auto,1,transform,3" ];
+  wayland.windowManager.hyprland.settings.monitor = [ "DSI-1,preferred,auto,1,transform,3" ];
 
   # Fix touch screen in hyprland
-  wayland.windowManager.hyprland.settings.input.touchdevice ={
+  wayland.windowManager.hyprland.settings.input.touchdevice = {
     enabled = true;
     transform = "3";
   };
+
+  # Battery in waybar (chuwi has a battery, ace doesn't)
+  programs.waybar.settings.mainBar = {
+    modules-right = [
+      "custom/left-arrow-dark"
+      "pulseaudio"
+      "custom/left-arrow-light"
+      "custom/left-arrow-dark"
+      "memory"
+      "custom/left-arrow-light"
+      "custom/left-arrow-dark"
+      "cpu"
+      "custom/left-arrow-light"
+      "custom/left-arrow-dark"
+      "battery"
+      "custom/left-arrow-light"
+      "custom/left-arrow-dark"
+      "disk"
+      "custom/left-arrow-light"
+      "custom/left-arrow-dark"
+      "tray"
+    ];
+    battery = {
+      states = {
+        good = 95;
+        warning = 30;
+        critical = 15;
+      };
+      format = "{icon} {capacity}%";
+      format-icons = [ "" "" "" "" "" ];
+    };
+  };
+  programs.waybar.style = ''
+    #battery {
+      color: #a6e3a1;
+    }
+  '';
 }
